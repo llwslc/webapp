b6cc3e15c28484531bfe7072a8c1d93b2bb24a9f
llwslc <llwslc@gmail.com>
2017-06-15 01:32:00 +0800

    init client

---
+++ template/client/src/components/pages/helloPage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <span>{{ hello }}</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    mixins: [Locale],
+
+    computed: {
+    },
+
+    data() {
+      return {
+        hello: Hello.hello
+      }
+    },
+
+    methods: {
+    },
+
+    updated() {
+    },
+
+    mounted() {
+    },
+  };
+</script>
+
+<style lang="scss">
+  @import "./assets/style/main-style";
+</style>
---
+++ template/client/src/vuex/store.js
@@ -0,0 +1,18 @@
+
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    // 配置
+    config: {},
+  },
+  mutations: {
+    init: state =>
+    {
+      state.config = {};
+    },
+  },
+})
