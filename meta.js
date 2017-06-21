b34559011dcac0ff844f33dae063f502524d33d7
llwslc <llwslc@gmail.com>
2017-06-21 17:05:26 +0800

    del completeMessage

--- meta.js
+++ meta.js
@@ -45,21 +45,17 @@ module.exports = {
     {
       fs.renameSync(oldPath, newPath);
     }
+
+    console.log(`---
+
+  All set. More configurations can be made at \x1b[33m{{destDirName}}/config.js\x1b[0m.
+
+  Next steps:
+    1.
+       \x1B[32mcd {{destDirName}}\x1b[0m
+    2.
+       \x1B[32mnpm i\x1b[0m
+    3.
+       \x1B[32mnpm run dev\x1b[0m`)
   },
-  completeMessage: `---
-
-All set. More configurations can be made at \x1b[33m{{destDirName}}/config.js\x1b[0m.
-
-Next steps:
-  1.
-     \x1B[32mcd {{destDirName}}\x1b[0m
-  2.
-     \x1B[32mnpm i\x1b[0m
-  3.
-     If \x1B[1mrebuild\x1b[0m flag is true. Need to be modified at \x1b[33m{{destDirName}}/tasks/install.js\x1b[0m.
-     If \x1B[1mfork\x1b[0m flag is true. Need to be modified at \x1b[33m{{destDirName}}/app/src/sections/forkJs.js\x1b[0m.
-     If \x1B[1mupdate\x1b[0m flag is true. Need to be modified at \x1b[33m{{destDirName}}/app/electron.js:57 (update.setFeedURL)\x1b[0m.
-     If \x1B[1minstaller\x1b[0m flag is true. Need to be modified at \x1b[33m{{destDirName}}/tasks/win/vdprojConfig.json\x1b[0m.
-  4.
-     \x1B[32mnpm run dev\x1b[0m`
 }
