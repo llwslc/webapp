7353f4cf58cf31d0e38856b3bd2becfa686d8109
llwslc <llwslc@gmail.com>
2017-06-30 14:28:04 +0800

    update mongoose connect interface

--- template/server/mongoose/db.js
+++ template/server/mongoose/db.js
@@ -7,7 +7,7 @@ var mongodbDao = function ()
   let self = this;
 
   self.dbUri = `mongodb://${config.mongodb.bindIp}:${config.mongodb.port}/${config.mongodb.db}`;
-  self.dbConn = mongoose.connect(self.dbUri, function (err)
+  self.dbConn = mongoose.connect(self.dbUri, {useMongoClient: true}, function (err)
   {
     if (err)
     {
